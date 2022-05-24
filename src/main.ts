264fe4f6e144d176fe3dd9be0ae05a8fa4b74d3e
stefan.kendlbacher <phhjmgxtvbmays3doirhq27m7juea6rrvqfctullf4qxjkxnxrgq>
2022-05-24 21:16:08 +0200

    added fastify adapter and swagger support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {FastifyAdapter, NestFastifyApplication} from "@nestjs/platform-fastify";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+
+    const config = new DocumentBuilder()
+        .setTitle('Forget Things')
+        .setDescription('The forget things API description')
+        .setVersion('1.0')
+        .addTag('cats')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
+
+    await app.listen(3000);
 }
+
 bootstrap();
